0a643825748278ea341729f03bbb385ba1fada47
Rafael Ferri <rafael.ferri@nubasolutions.com>
2023-05-08 14:46:36 -0400

    Changed sidebar position

--- components/Sidebars/SidebarOne.tsx
+++ components/Sidebars/SidebarOne.tsx
@@ -4,7 +4,7 @@ import SidebarListItem from '../ListItems/SidebarListItem'
 const SidebarOne = ({isSidebarOpen} : any) => {
 
     return (
-        <aside className={`fixed bottom-0 z-40 ${isSidebarOpen ? 'left-0' : '-left-full -translate-x-full'} lg:sticky lg:translate-x-0 lg:left-0 w-[250px] h-[calc(100vh-70px)] pt-3 transition-transform bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700`} aria-label="Sidebar">
+        <aside className={`fixed top-[70px] z-40 ${isSidebarOpen ? 'left-0' : '-left-full -translate-x-full'} lg:sticky lg:translate-x-0 lg:left-0 w-[250px] h-[calc(100vh-70px)] pt-3 transition-transform bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700`} aria-label="Sidebar">
             <div className="h-full w-[250px] px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700">
                 <ul className="space-y-2 font-medium">
                     <SidebarListItem name='Dashboard' iconLeft={<IoPieChart/>} link='dashboard' iconRight={<IoChevronDown/>}/>
